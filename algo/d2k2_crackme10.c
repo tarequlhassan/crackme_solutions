68603e00c3ebc4a18b8f46a023807dc3896fd72f
mudlord <mudlord@rebote.net>
2019-12-11 16:23:31 +1100

    Completed number 10. All of diablo2oo2's crackmes are now complete.

--- algo/d2k2_crackme10.c
+++ algo/d2k2_crackme10.c
@@ -4,7 +4,106 @@
 #include <stdint.h>
 #include <intrin.h>
 #include <math.h>
-#include "d2k2_crackme10_base64.h"
+#include <ctype.h>
+
+static const char base64digits[] =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+#define BAD -1
+
+static const char base64val[] = {
+	BAD,BAD,BAD,BAD, BAD,BAD,BAD,BAD, BAD,BAD,BAD,BAD, BAD,BAD,BAD,BAD,
+	BAD,BAD,BAD,BAD, BAD,BAD,BAD,BAD, BAD,BAD,BAD,BAD, BAD,BAD,BAD,BAD,
+	BAD,BAD,BAD,BAD, BAD,BAD,BAD,BAD, BAD,BAD,BAD, 62, BAD,BAD,BAD, 63,
+	 52, 53, 54, 55,  56, 57, 58, 59,  60, 61,BAD,BAD, BAD,BAD,BAD,BAD,
+	BAD,  0,  1,  2,   3,  4,  5,  6,   7,  8,  9, 10,  11, 12, 13, 14,
+	 15, 16, 17, 18,  19, 20, 21, 22,  23, 24, 25,BAD, BAD,BAD,BAD,BAD,
+	BAD, 26, 27, 28,  29, 30, 31, 32,  33, 34, 35, 36,  37, 38, 39, 40,
+	 41, 42, 43, 44,  45, 46, 47, 48,  49, 50, 51,BAD, BAD,BAD,BAD,BAD
+};
+
+#define DECODE64(c)  (isascii(c) ? base64val[c] : BAD)
+
+/* Encode into base64 */
+void base64_encode(unsigned char* out, const unsigned char* in, int inlen)
+{
+	/* raw bytes in quasi-big-endian order to base 64 string (NUL-terminated) */
+	for (; inlen >= 3; inlen -= 3) {
+		*out++ = base64digits[in[0] >> 2];
+		*out++ = base64digits[((in[0] << 4) & 0x30) | (in[1] >> 4)];
+		*out++ = base64digits[((in[1] << 2) & 0x3c) | (in[2] >> 6)];
+		*out++ = base64digits[in[2] & 0x3f];
+		in += 3;
+	}
+
+	if (inlen > 0) {
+		unsigned char fragment;
+
+		*out++ = base64digits[in[0] >> 2];
+		fragment = (in[0] << 4) & 0x30;
+		if (inlen > 1)
+			fragment |= in[1] >> 4;
+		*out++ = base64digits[fragment];
+		*out++ = (inlen < 2) ? '=' : base64digits[(in[1] << 2) & 0x3c];
+		*out++ = '=';
+	}
+	*out = '\0';
+}
+
+/*
+ * Decode from base64.
+ *
+ * base 64 to raw bytes in quasi-big-endian order, returning count of bytes
+ * maxlen limits output buffer size, set to zero to ignore.
+ */
+int base64_decode(unsigned char* out, const unsigned char* in, int maxlen)
+{
+	register unsigned char digit1, digit2, digit3, digit4;
+	int len = 0;
+
+	if (in[0] == '+' && in[1] == ' ')
+		in += 2;
+	if (*in == '\r')
+		return(0);
+
+	do {
+		digit1 = in[0];
+		if (DECODE64(digit1) == BAD)
+			return(-1);
+		digit2 = in[1];
+		if (DECODE64(digit2) == BAD)
+			return(-1);
+		digit3 = in[2];
+		if (digit3 != '=' && DECODE64(digit3) == BAD)
+			return(-1);
+		digit4 = in[3];
+		if (digit4 != '=' && DECODE64(digit4) == BAD)
+			return(-1);
+		in += 4;
+		++len;
+
+		if (maxlen && len > maxlen)
+			return(-1);
+
+		*out++ = (DECODE64(digit1) << 2) | (DECODE64(digit2) >> 4);
+		if (digit3 != '=')
+		{
+			++len;
+			if (maxlen && (len > maxlen))
+				return(-1);
+
+			*out++ = ((DECODE64(digit2) << 4) & 0xf0) | (DECODE64(digit3) >> 2);
+			if (digit4 != '=') {
+				++len;
+				if (maxlen && (len > maxlen))
+					return(-1);
+				*out++ = ((DECODE64(digit3) << 6) & 0xc0) | DECODE64(digit4);
+			}
+		}
+	} while (*in && (*in != '\r') && (digit4 != '='));
+
+	return(len);
+}
 
 typedef union
 {
@@ -129,24 +228,6 @@ void process_serial(char* name, char* serial_out)
 		int l = buf1[k + 1] & 0x0F;
 		buf2[i] = j+l;
 	}
-		
-	//just some test data.
-	//char *basedec = "bXVkbG9yZGlzZ3JlYXQhP0JCQkJCQkJCQkJCQkJCQkJBQUFBQUFBQUFBQUFBQUFBQ0NDQ0NDQ0NDQ0NDQ0NDQ0RERERERERERERERERERERFRUVFRUVFRUVFRUVFRUVFRkZGRkZGRkZGRkZGRkZGRjEyMzQ1Njc4OTBBQkNERUY=";
-	//BYTE b64enc[255] = { 0 };
-
-	
-
-	//base64_decode(b64enc, basedec, 0);
-	
-	/*int64_t* base64_ptr = b64enc;
-	__m128i buf2_mask_mmx = _mm_loadl_epi64(buf2);
-	for (int i = 0; i < 16; i++)
-	{
-		__m128i var1_mmx = _mm_loadl_epi64(base64_ptr);
-	    var1_mmx= _mm_xor_si128(buf2_mask_mmx, var1_mmx);
-		_mm_storeu_si64(base64_ptr, var1_mmx);
-		base64_ptr++;
-	}*/
 
 	for (int i = 0; i < 16; i++)
 	{
@@ -157,30 +238,32 @@ void process_serial(char* name, char* serial_out)
 	soduku[i][i] = buf1[i];
 	int flag = 0;
 	sodu_solve(0, 0,&flag);
-
-
+	
 
 	BYTE b64encstr[128] = { 0 };
-	base64_encode(b64encstr, soduku, 0x80);
+		unsigned char* in = soduku;
+		unsigned char* out = b64encstr;
+		for (int i = 0,  k=0; i < sizeof(b64encstr); i++,k+=2)
+		{
+			int j = in[k ] << 4;
+			int l = in[k +1] & 0x0F;
+			out[i] = j + l;
+		}
 
 
-
-	/*
-	BYTE buf3[0x80] = { 0 };
-	for (int i = 0; i < 0x80; i++)
+	int64_t* base64_ptr = b64encstr;
+	__m128i buf2_mask_mmx = _mm_loadl_epi64(buf2);
+	for (int i = 0; i < 16; i++)
 	{
-		Register EAX, EDX;
-		EAX.ex = b64enc[i];
-		EDX.ex = EAX.ex;
-		EAX.b.lo &= 0x0F;
-		EDX.b.lo >>= 0x04;
-		buf3[i*2] = EDX.b.lo;
-		buf3[i*2 + 1] = EAX.b.lo;
-	}*/
+	__m128i var1_mmx = _mm_loadl_epi64(base64_ptr);
+	var1_mmx= _mm_xor_si128(buf2_mask_mmx, var1_mmx);
+	_mm_storeu_si64(base64_ptr, var1_mmx);
+	base64_ptr++;
+	}
 
-	//base64_encode(b64enc, "mudlordisgreat", strlen("mudlordisgreat"));
-	
-	wsprintf(serial_out,"%s", b64encstr);
+	BYTE b64encstr2[256] = { 0 };
+	base64_encode(b64encstr2, b64encstr, 0x80);
+	wsprintf(serial_out,"%s", b64encstr2);
 
 	
 }
