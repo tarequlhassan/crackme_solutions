7154185edcdfa756eac41bf6f4c15380eff1f8c6
mudlord <mudlord@rebote.net>
2020-02-21 13:39:00 +1100

    Table seems to match original crackme, might need to extend it a bit as needed for larger serials over 7 characters in size.

--- algo/zuma_crackme3.c
+++ algo/zuma_crackme3.c
@@ -7,17 +7,31 @@ extern void _stdcall mars_encrypt(DWORD* input, DWORD* output);
 void init() {}
 
 void process_serial(char *name, char *serial_out) {
+  int namelen = lstrlen(name);
   char* mars_key = "FEABCBFFFF183461";
   mars_setkey(mars_key, strlen(mars_key));
-  char namestr[0x10] = {0}; //pad out blocksize to match block coming out, sym crypt 101
-  char mars_out[0x10] = { 0 };
-  int namelen = lstrlen(name);
+  //pad out blocksize to match block coming out, sym crypt 101
+  char namestr[0x10] = {0}; 
+  char mars_out[0x20] = { 0 };
+  char mars_tbl[0x40] = { 0 };
   int namesum = 0;
-
   for (int i = 0; i < namelen; i++)
       namesum += name[i];
   wsprintf(namestr, "%lX", namesum);
+
   mars_encrypt(namestr, mars_out);
+
+  DWORD* mars_ptr = mars_out;
+  DWORD marsstrptr = mars_tbl;
+  for (int i = 0; i < 0x20; i += 4)
+  {
+      wsprintf(&mars_tbl[i*2], "%d", *mars_ptr);
+      mars_ptr++;
+  }
+
+  int marstbloutlen = strlen(mars_tbl);
+
+ 
 
   wsprintf(serial_out, "you got my love");
 }
