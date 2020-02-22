26a63eea1fbc3ee19777459d5dec250832e6f5cb
mudlord <mudlord@rebote.net>
2020-02-22 18:54:00 +1100

    prelim. solution, needs bugchecks.

--- algo/zuma_crackme3.c
+++ algo/zuma_crackme3.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <windows.h>
+#include <stdint.h>
 
 extern void _stdcall mars_setkey(DWORD* input, DWORD input_len);
 extern void _stdcall mars_encrypt(DWORD* input, DWORD* output);
@@ -13,7 +14,9 @@ void process_serial(char *name, char *serial_out) {
   //pad out blocksize to match block coming out, sym crypt 101
   char namestr[0x10] = {0}; 
   char mars_out[0x20] = { 0 };
-  char mars_tbl[0x40] = { 0 };
+  char mars_tbl[0x50] = { 0 };
+
+  char magic[0x6] = { 0 };
   int namesum = 0;
   for (int i = 0; i < namelen; i++)
       namesum += name[i];
@@ -22,16 +25,26 @@ void process_serial(char *name, char *serial_out) {
   mars_encrypt(namestr, mars_out);
 
   DWORD* mars_ptr = mars_out;
-  DWORD marsstrptr = mars_tbl;
+  BYTE* marsstrptr = mars_tbl;
+  marsstrptr += 6;
   for (int i = 0; i < 0x20; i += 4)
   {
-      wsprintf(&mars_tbl[i*2], "%d", *mars_ptr);
+      wsprintf(&marsstrptr[i*2], "%d", *mars_ptr);
       mars_ptr++;
   }
 
-  int marstbloutlen = strlen(mars_tbl);
+  int marstbloutlen = strlen(marsstrptr);
+  magic[0] = mars_tbl[5 + marstbloutlen];
+  magic[1] = mars_tbl[4 + marstbloutlen];
+  magic[2] = mars_tbl[3 + marstbloutlen];
+  magic[3] = mars_tbl[1 + marstbloutlen];
+  magic[4] = mars_tbl[marstbloutlen];
+
+  //solving this is fucking pointless.
+  //uint16_t shit2 = ( 'E' ^ 'S') * 0x4B32;
+  //it all comes out the same every single time.
 
  
 
-  wsprintf(serial_out, "you got my love");
+  wsprintf(serial_out, "%s-55600",magic);
 }
