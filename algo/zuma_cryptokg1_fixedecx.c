9b1a4c4e302573b181de0ce3cd4aece3305b8880
mudlord <mudlord@protonmail.com>
2020-05-21 07:47:25 +1000

    decoded 1st loop.

--- algo/zuma_cryptokg1_fixedecx.c
+++ algo/zuma_cryptokg1_fixedecx.c
@@ -18,11 +18,19 @@ void process_serial(char* name, char* serial_out) {
     BYTE hash_bytes[16] = { 0 };
     int namelen = lstrlen(name);
 
-    int ecx_ = 0;
-    int eax_ = 0xBEEFC0DE;
-    ecx_ += eax_;
-    eax_ =_byteswap_ulong(eax_);
-    ecx_ += (eax_*6);
+    Register eax_,edx_;
+    eax_.ex = namelen;
+    edx_.ex = namelen;
+
+
+    for (int i = 0; i < namelen; i++)
+    {
+        eax_.b.lo += name[i];
+        eax_.ex += 0x7C80BDF6;
+        eax_.ex *= edx_.ex;
+        eax_.ex += eax_.ex;
+        edx_.ex--;
+    }
    
     wsprintf(serial_out, "fuck!");
 }
