4bcbf954b78ca095a3df7ebf39bedd5c8e1e3aff
mudlord <mudlord@protonmail.com>
2020-05-21 07:27:46 +1000

    sketch out initial part. seems to be useless shit since ECX is set down the path anyway.

--- algo/zuma_cryptokg1_fixedecx.c
+++ algo/zuma_cryptokg1_fixedecx.c
@@ -2,12 +2,27 @@
 #include <stdint.h>
 #include <windows.h>
 
+typedef union {
+    uint64_t rx;
+    uint32_t ex;
+    uint16_t x;
+    struct {
+        uint8_t lo, hi;
+    } b;
+} Register;
+
 
 void init() {}
 
 void process_serial(char* name, char* serial_out) {
     BYTE hash_bytes[16] = { 0 };
     int namelen = lstrlen(name);
+
+    int ecx_ = 0;
+    int eax_ = 0xBEEFC0DE;
+    ecx_ += eax_;
+    eax_ =_byteswap_ulong(eax_);
+    ecx_ += (eax_*6);
    
     wsprintf(serial_out, "fuck!");
 }
