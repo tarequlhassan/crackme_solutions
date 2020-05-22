c4711ce870eff95c178e8267c7a7efd4e44def3a
mudlord <mudlord@protonmail.com>
2020-05-22 19:41:36 +1000

    work through.

--- algo/zuma_cryptokg1_fixedecx.c
+++ algo/zuma_cryptokg1_fixedecx.c
@@ -15,22 +15,39 @@ typedef union {
 void init() {}
 
 void process_serial(char* name, char* serial_out) {
+    //each member of the lut equals a number
+    //so X = 1, Z = 2 and so on
     const char* lut = "XZMAJK";
-    BYTE hash_bytes[14] = { 0 };
+    char hash_bytes[16] = "XXZXAAJ-XZJXKXJ";
+
+
+    //zuma555
+    //XXZXAAJ - XZJXKXJ
+
     int namelen = lstrlen(name);
 
-    Register eax_,edx_;
-    eax_.ex = namelen;
-    edx_.ex = namelen;
+    Register hasheax_,namelen_ha;
+    hasheax_.ex = namelen_ha.ex = namelen;
 
-
+    //the hash
     for (int i = 0; i < namelen; i++)
     {
-        eax_.b.lo += name[i];
-        eax_.ex += 0x7C80BDF6;
-        eax_.ex *= edx_.ex;
-        eax_.ex += eax_.ex;
-        edx_.ex--;
+        hasheax_.b.lo += name[i];
+        hasheax_.ex += 0x7C80BDF6;
+        hasheax_.ex *= namelen_ha.ex;
+        hasheax_.ex += hasheax_.ex;
+        namelen_ha.ex--;
+    }
+    hasheax_.ex = hasheax_.b.lo * 2;
+
+    Register eax_ = { 0 }, edx_ = { 0 }, ebx_ = { 0 };
+
+    for (int i = 0; i < 5; i++)
+    {
+        int j = 0;
+        for (; j < strlen(lut); j++)
+            if (lut[j] == hash_bytes[i])break;
+
     }
 
     
