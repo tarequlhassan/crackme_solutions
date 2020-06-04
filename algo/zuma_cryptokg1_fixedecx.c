86c7dc7795104e17d2a173279c0dcdb65f56ee18
mudlord <mudlord@protonmail.com>
2020-06-04 20:00:30 +1000

    fix base-6 conversion/parsing.

--- algo/zuma_cryptokg1_fixedecx.c
+++ algo/zuma_cryptokg1_fixedecx.c
@@ -17,10 +17,8 @@ void init() {}
 void process_serial(char* name, char* serial_out) {
     //each member of the lut equals a number
     //so X = 1, Z = 2 and so on
-    const char* lut = "XZMAJK";
+    const char lut[6] = { 'X','Z','M','A','J','K' };
     char hash_bytes[16] = "XXZXAAJ-XZJXKXJ";
-
-
     //zuma555
     //XXZXAAJ - XZJXKXJ
 
@@ -29,7 +27,7 @@ void process_serial(char* name, char* serial_out) {
     Register hasheax_,namelen_ha;
     hasheax_.ex = namelen_ha.ex = namelen;
 
-    //the hash
+    //the checksum hash
     for (int i = 0; i < namelen; i++)
     {
         hasheax_.b.lo += name[i];
@@ -42,15 +40,27 @@ void process_serial(char* name, char* serial_out) {
 
     Register eax_ = { 0 }, edx_ = { 0 }, ebx_ = { 0 };
 
-    for (int i = 0; i < 5; i++)
-    {
-        int j = 0;
-        for (; j < strlen(lut); j++)
-            if (lut[j] == hash_bytes[i])break;
 
+    //first portion
+    for (int i = 0; i < namelen; i++)
+    {
+        for (int j = 0; j < 6; j++)
+            if (hash_bytes[i] == lut[j])
+            {
+                hash_bytes[i] = j;
+                break;
+            } 
     }
 
-    
-   
+    //second round
+    for (int i = 0; i < namelen; i++)
+    {
+        for (int j = 0; j < 6; j++)
+            if (hash_bytes[i+(namelen+1)] == lut[j])
+            {
+                hash_bytes[i +(namelen+1)] = j;
+                break;
+            }
+    }
     wsprintf(serial_out, "fuck!");
 }
